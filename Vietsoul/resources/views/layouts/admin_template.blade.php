5d1d496ceb06ad1a6b64712f45b7856beb4de381
Le Thuy Hang <thuyhangle.le@gmail.com>
2016-04-12 03:28:44 +0300

    reduce side bar code

--- Vietsoul/resources/views/layouts/admin_template.blade.php
+++ Vietsoul/resources/views/layouts/admin_template.blade.php
@@ -239,68 +239,23 @@
 
         <!--/. NAV TOP END  -->
 
-
+        <!-- /. NAV SIDE START -->
         <nav class="navbar-default navbar-side" role="navigation">
             <div class="sidebar-collapse">
                 <ul class="nav" id="main-menu">
+
                 @yield('sidebar')
-                    <!-- <li>
-                        <a href="{{ url('/admin_dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="{{ url('/admin_product') }}"><i class="fa fa-desktop"></i> Product </a>
-                    </li>
-					<li>
-                        <a href="{{ url('/admin_newOrders') }}"><i class="fa fa-bar-chart-o"></i> Orders </a>
-                    </li>
-                    <li>
-                        <a href="{{ url('/admin_customer') }}"><i class="fa fa-qrcode"></i> Customer Account</a>
-                    </li>
 
-                    <li>
-                        <a href="{{ url('/admin_message') }}"><i class="fa fa-table"></i> Messages </a>
-                    </li>
-                    <li>
-                        <a href="{{ url('/admin_faq') }}"><i class="fa fa-edit"></i> FAQ Update </a>
-                    </li>
-
-                    <li>
-                        <a href="#"><i class="fa fa-sitemap"></i> Bla<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="#">bla</a>
-                            </li>
-                            <li>
-                                <a href="#">bla</a>
-                            </li>
-                            <li>
-                                <a href="#">bla<span class="fa arrow"></span></a>
-                                <ul class="nav nav-third-level">
-                                    <li>
-                                        <a href="#"> Link</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"> Link</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"> Link</a>
-                                    </li>
-
-                                </ul>
-
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a class="active-menu" href="empty.html"><i class="fa fa-fw fa-file"></i> Empty Page</a>
-                    </li> -->
                 </ul>
 
             </div>
 
         </nav>
-        <!-- /. NAV SIDE  -->
+        <!-- /. NAV SIDE END -->
+
+        <!-- /. PAGE WRAPPER START -->
         <div id="page-wrapper" >
+
             <div id="page-inner">
 			    <div class="row">
                     <div class="col-md-12">
@@ -313,12 +268,14 @@
 
                  <!-- /. ROW  -->
 				 <footer><p> © 2016 LinhHangTung <a href="{{ url('/') }}">VietSoul Store</a></p></footer>
-				</div>
-             <!-- /. PAGE INNER  -->
-            </div>
-         <!-- /. PAGE WRAPPER  -->
+			</div>
+
         </div>
+         <!-- /. PAGE WRAPPER END -->
+
+    </div>
      <!-- /. WRAPPER  -->
+
 
     <!-- JS Scripts-->
 
