dfbf6a70261dfb37e815fd5fcfaffc548e01a87a
Le Thuy Hang <thuyhangle.le@gmail.com>
2016-04-16 04:34:38 +0300

    add tables, chart on dashboard

--- Vietsoul/resources/views/admin_dashboard.blade.php
+++ Vietsoul/resources/views/admin_dashboard.blade.php
@@ -28,48 +28,19 @@
         <a href="{{ url('/admin_faq') }}"><i class="fa fa-edit"></i> FAQ Update </a>
     </li>
 
-    <li>
-        <a href="#"><i class="fa fa-sitemap"></i> Bla<span class="fa arrow"></span></a>
-        <ul class="nav nav-second-level">
-            <li>
-                <a href="#">bla</a>
-            </li>
-            <li>
-                <a href="#">bla</a>
-            </li>
-            <li>
-                <a href="#">bla<span class="fa arrow"></span></a>
-                <ul class="nav nav-third-level">
-                    <li>
-                        <a href="#"> Link</a>
-                    </li>
-                    <li>
-                        <a href="#"> Link</a>
-                    </li>
-                    <li>
-                        <a href="#"> Link</a>
-                    </li>
-
-                </ul>
-
-            </li>
-        </ul>
-    </li>
-    <li>
-        <a class="active-menu" href="empty.html"><i class="fa fa-fw fa-file"></i> Empty Page</a>
-    </li>
 @endsection
 
 @section ('content')
 
-<!-- Cards for counting new thing start -->
-    <div class="row">
+<!-- VScards for counting new thing start -->
+    <div class="card row">
+
         <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 animated pulse">
             <a href="{{ url('/admin_message') }}">
-                <div class="card blue summary-inline">
-                    <div class="card-body">
+                <div class="card VSblue summary-inline">
+                    <div class="VScard-body">
                         <i class="icon fa fa-comments fa-4x"></i>
-                        <div class="card-content">
+                        <div class="VScard-content">
                             <div class="title"> {{ $message }} </div>
                             <div class="sub-title">New Messages</div>
                         </div>
@@ -81,10 +52,10 @@
 
         <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 animated pulse">
             <a href="{{ url('/admin_newOrders') }}">
-                <div class="card yellow summary-inline">
-                    <div class="card-body">
+                <div class="card VSyellow summary-inline">
+                    <div class="VScard-body">
                         <i class="icon fa fa-bar-chart-o fa-4x"></i>
-                        <div class="card-content">
+                        <div class="VScard-content">
                             <div class="title"> {{ $order }} </div>
                             <div class="sub-title">New Orders</div>
                         </div>
@@ -96,10 +67,10 @@
 
         <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 animated pulse">
             <a href="{{ url('/admin_customer') }}">
-                <div class="card green summary-inline">
-                    <div class="card-body">
+                <div class="card VSgreen summary-inline">
+                    <div class="VScard-body">
                         <i class="icon fa fa-users fa-4x"></i>
-                        <div class="card-content">
+                        <div class="VScard-content">
                             <div class="title"> {{ $user }} </div>
                             <div class="sub-title">New Customers</div>
                         </div>
@@ -111,10 +82,10 @@
 
         <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 animated pulse">
             <a href="{{ url('/admin_product') }}">
-                <div class="card red summary-inline">
-                    <div class="card-body">
+                <div class="card VSred summary-inline">
+                    <div class="VScard-body">
                         <i class="icon fa fa-tag fa-4x"></i>
-                        <div class="card-content">
+                        <div class="VScard-content">
                             <div class="title"> {{ $total }} </div>
                             <div class="sub-title">New Products</div>
                         </div>
@@ -125,8 +96,168 @@
         </div>
 
     </div>
-<!-- Cards for counting new thing end -->
+<!-- VScards for counting new thing end -->
 
+<!-- chart -->
+    <div class="card col-sm-3">
+        <div class="card-image waves-effect waves-block waves-light">
+            <canvas id="vs-chart" width="400" height="400"></canvas>
+        </div>
+        <div class="card-content" align="center">
+            <span class="card-title activator grey-text text-darken-4">Total: {{ $total }} Products</span>
+        </div>
+
+        <script>
+            var vsChart = document.getElementById("vs-chart").getContext("2d");
+            var options = {
+                scaleLineWidth: 1,
+                showScale: true,
+                responsive: true,
+                scaleFontSize: 20
+            };
+
+            var data = [
+                {
+                    value: parseFloat({{ $percentclot }}),
+                    color:"#e53935",
+                    highlight: "#ffcdd2",
+                    label: "Clothing"
+                },
+                {
+                    value: parseFloat({{ $percentacc }}),
+                    color: "#d81b60",
+                    highlight: "#f8bbd0",
+                    label: "Accessories"
+                },
+                {
+                    value: parseFloat({{ $percenttoy }}),
+                    color: "#8e24aa",
+                    highlight: "#e1bee7",
+                    label: "Toys"
+                },
+                {
+                    value: parseFloat({{ $percentoth }}),
+                    color: "#5e35b1 ",
+                    highlight: "#d1c4e9",
+                    label: "Others"
+                },
+                {
+                    value: parseFloat({{ $percentart }}),
+                    color: "#3949ab",
+                    highlight: "#c5cae9",
+                    label: "Artworks"
+                }
+            ];
+
+            var myDoughnutChart = new Chart(vsChart).Doughnut(data,options);
+        </script>
+    </div>
+
+    <div class="card col-sm-3">
+        <div class="card-image waves-effect waves-block waves-light">
+            <canvas id="vs-pie-chart" width="400" height="400"></canvas>
+        </div>
+        <div class="card-content" align="center">
+            <span class="card-title activator grey-text text-darken-4">Best Sold: {{ $bestproduct}}</span>
+        </div>
+
+        <script>
+            var vsPieChart = document.getElementById("vs-pie-chart").getContext("2d");
+            var options = {
+                scaleLineWidth: 1,
+                showScale: true,
+                responsive: true,
+                scaleFontSize: 20
+            };
+
+            var data = [
+                {
+                    value: parseFloat({{ $bestsold }}),
+                    color:"#039be5 ",
+                    highlight: "#4fc3f7",
+                    label: "{{ $bestproduct}}"
+                },
+                {
+                    value: 100 - parseFloat({{ $bestsold }}),
+                    color: "#b3e5fc",
+                    highlight: "#e1f5fe",
+                    label: "Other Items"
+                }
+            ];
+
+            var myPieChart = new Chart(vsPieChart).Pie(data,options);
+        </script>
+    </div>
+
+    <div class="card col-md-5 db-table">
+        <table class="responsive-table">
+            <thead>
+                <tr>
+                    <th data-field="sold">Total Sold</th>
+                    <th data-field="profit">Profit</th>
+                    <th data-field="cost">Cost</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td> $ {{ $sold }} </td>
+                    <td> $ {{ $profit}} </td>
+                    <td> $ {{ $cost }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+<!-- end chart -->
+
+    <div class="card col-md-5 db-table">
+        <table class="responsive-table">
+            <thead>
+                <tr>
+                    <th data-field="name">Name</th>
+                    <th data-field="amount">Amount</th>
+                    <th data-field="percentage">Percentage</th>
+                </tr>
+            </thead>
+
+            <tbody>
+
+                <tr>
+                    <td> Clothing </td>
+                    <td> {{ $numclot }} </td>
+                    <td> {{ $percentclot }}% </td>
+                </tr>
+                <tr>
+                    <td> Accessories</td>
+                    <td> {{ $numacc }} </td>
+                    <td> {{ $percentacc }}% </td>
+                </tr>
+                <tr>
+                    <td> Toys </td>
+                    <td> {{ $numtoy }} </td>
+                    <td> {{ $percenttoy }}% </td>
+                </tr>
+                <tr>
+                    <td> Artwork </td>
+                    <td> {{ $numart }} </td>
+                    <td> {{ $percentart }}% </td>
+                </tr>
+                <tr>
+                    <td> Others </td>
+                    <td> {{ $numoth }} </td>
+                    <td> {{ $percentoth }}% </td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+
+@endsection
+
+
+<!--
     Message count: {{ $message }}
     <br>
     Order count: {{ $order }}
@@ -148,7 +279,7 @@
     <br>
     Total : {{ $total }} product(s)
     <br>
-    Clothing : {{ $numclot }} product(s) , {{ $percentclot}}% of total products
+    Clothing : {{ $numclot }} product(s) , {{ $percentclot }}% of total products
     <br>
     Accessories : {{ $numacc }} product(s) , {{ $percentacc }}% of total products
     <br>
@@ -157,11 +288,7 @@
     Artworks : {{ $numart }} product(s) , {{ $percentart }}% of total products
     <br>
     Others : {{ $numoth }} product(s) , {{ $percentoth }}% of total products
-    <br>
-@endsection
-
-
-
+    <br> -->
    <!--
     Message count: {{ $message }}
     <br>
--- Vietsoul/resources/views/layouts/admin_template.blade.php
+++ Vietsoul/resources/views/layouts/admin_template.blade.php
@@ -6,6 +6,8 @@
     <title>VietSoul Admin Page</title>
 	<!-- Bootstrap Styles-->
     <link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet" />
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
     <!-- FontAwesome Styles-->
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <!-- Animation Styles-->
@@ -14,6 +16,7 @@
     <link href="assets/css/admin-styles.css" rel="stylesheet" />
      <!-- Google Fonts-->
    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.1.1/Chart.min.js"></script>
 
 </head>
 <body>
@@ -30,195 +33,7 @@
             </div>
 
             <ul class="nav navbar-top-links navbar-right">
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
-                        <i class="fa fa-envelope fa-fw"></i> <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-messages">
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <strong>John Doe</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>Today</em>
-                                    </span>
-                                </div>
-                                <div>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...</div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <strong>John Smith</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-                                </div>
-                                <div>Lorem Ipsum has been the industry's standard dummy text ever since an kwilnw...</div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <strong>John Smith</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-                                </div>
-                                <div>Lorem Ipsum has been the industry's standard dummy text ever since the...</div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a class="text-center" href="#">
-                                <strong>Read All Messages</strong>
-                                <i class="fa fa-angle-right"></i>
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-messages -->
-                </li>
-                <!-- /.dropdown -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
-                        <i class="fa fa-tasks fa-fw"></i> <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-tasks">
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <p>
-                                        <strong>Task 1</strong>
-                                        <span class="pull-right text-muted">60% Complete</span>
-                                    </p>
-                                    <div class="progress progress-striped active">
-                                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-                                            <span class="sr-only">60% Complete (success)</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <p>
-                                        <strong>Task 2</strong>
-                                        <span class="pull-right text-muted">28% Complete</span>
-                                    </p>
-                                    <div class="progress progress-striped active">
-                                        <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="28" aria-valuemin="0" aria-valuemax="100" style="width: 28%">
-                                            <span class="sr-only">28% Complete</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <p>
-                                        <strong>Task 3</strong>
-                                        <span class="pull-right text-muted">60% Complete</span>
-                                    </p>
-                                    <div class="progress progress-striped active">
-                                        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-                                            <span class="sr-only">60% Complete (warning)</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <p>
-                                        <strong>Task 4</strong>
-                                        <span class="pull-right text-muted">85% Complete</span>
-                                    </p>
-                                    <div class="progress progress-striped active">
-                                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 85%">
-                                            <span class="sr-only">85% Complete (danger)</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a class="text-center" href="#">
-                                <strong>See All Tasks</strong>
-                                <i class="fa fa-angle-right"></i>
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-tasks -->
-                </li>
-                <!-- /.dropdown -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
-                        <i class="fa fa-bell fa-fw"></i> <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-alerts">
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <i class="fa fa-comment fa-fw"></i> New Comment
-                                    <span class="pull-right text-muted small">4 min</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <i class="fa fa-twitter fa-fw"></i> 3 New Followers
-                                    <span class="pull-right text-muted small">12 min</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <i class="fa fa-envelope fa-fw"></i> Message Sent
-                                    <span class="pull-right text-muted small">4 min</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <i class="fa fa-tasks fa-fw"></i> New Task
-                                    <span class="pull-right text-muted small">4 min</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <i class="fa fa-upload fa-fw"></i> Server Rebooted
-                                    <span class="pull-right text-muted small">4 min</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a class="text-center" href="#">
-                                <strong>See All Alerts</strong>
-                                <i class="fa fa-angle-right"></i>
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-alerts -->
-                </li>
+
                 <!-- /.dropdown -->
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
@@ -238,7 +53,7 @@
         <!--/. NAV TOP END  -->
 
         <!-- /. NAV SIDE START -->
-        <nav class="navbar-default navbar-side" role="navigation">
+        <div class="navbar-default navbar-side" role="navigation">
             <div class="sidebar-collapse">
                 <ul class="nav" id="main-menu">
 
@@ -248,7 +63,7 @@
 
             </div>
 
-        </nav>
+        </div>
         <!-- /. NAV SIDE END -->
 
         <!-- /. PAGE WRAPPER START -->
@@ -256,12 +71,12 @@
 
             <div id="page-inner">
 			    <div class="row">
-                    <div class="col-md-12">
                         <h1 class="page-header">
                             @yield('title')<small>@yield('subtitle')</small>
                         </h1>
-                    </div>
+                    <div>
                     @yield('content')
+                    </div>
                 </div>
 
                  <!-- /. ROW  -->
@@ -287,7 +102,6 @@
     <script src="assets/js/jquery.metisMenu.js"></script>
       <!-- Custom Js -->
     <script src="assets/js/admin-scripts.js"></script>
-
 
 </body>
 </html>
