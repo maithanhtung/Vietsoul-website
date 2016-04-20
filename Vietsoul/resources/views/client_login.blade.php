c4dd44a65eb75859abd784ddbf98d9589c075159
Le Thuy Hang <thuyhangle.le@gmail.com>
2016-04-20 22:55:07 +0300

    remove some necessary pages

--- Vietsoul/resources/views/client_login.blade.php
+++ Vietsoul/resources/views/client_login.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    You have logged in!
                 </div>
             </div>
         </div>
